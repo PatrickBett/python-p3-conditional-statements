eb14226b62f4095ab3e99e17b8365b67ff4ec9e3
PatrickBett <patrickbett018@gmail.com>
2023-11-24 23:59:03 +0300

    first commit

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,11 +2,23 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username=="ADMIN" or username=="admin") and (password=='12345'):
+        return 'Access Granted'
+    else:
+        return "Access Denied"
+
+
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if (temperature<40):
+        return "It's brisk out there!"
+    elif (temperature < 65 and temperature >39):
+        return "It's a little chilly out there!"
+    elif (temperature >85):
+        return  "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
     # your code here
